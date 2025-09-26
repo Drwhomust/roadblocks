e01682054bada4aadc809372c0592f504926a5b2
Drwhomust <rabbids8008@gmail.com>
2025-09-26 14:04:11 -0700

    Update luau.lua

--- studio/luau.lua
+++ studio/luau.lua
@@ -1,6 +1,7 @@
 --[[
 This is a Love2D library that port's roblox's luau to Love2D
 This also uses a 3D library called brinevector3D https://github.com/flamendless/brinevector3D
+And dump-3dpd https://github.com/oniietzschan/bump-3dpd
 
 i tired my best on this port. please don't hasses me on my code
 ]]--
@@ -15,5 +16,16 @@ function love.load()
   local bump = require 'bump-3dpd'
 end
 
+-- everment
 local workspace = {}
-local workspace.a = "to prevent memory clean up"
+local workspace.z = "to prevent memory clean up"
+
+local repcaltedstoage = {}
+local repcaltedstoage.z = "to prevent memort clean up"
+
+local Instance = {}
+
+-- TODO find a way for people to overwrite this and find a better way to do this
+
+-- server
+local ServerStoage = {}
